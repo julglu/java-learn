f12e897eaae939ff72016bfecb46d64133059be2
julglu <jan-glu@yandex.ru>
2017-04-12 16:11:29 +0400

    Домашнее задание к Занятию 12 (Map) 1)Топ-100 наиболее часто исоьзуемых слов 2)Собрать все слова в группы по их длине 3)Топ-10 самых популярных сочетаний 2-х слов 4)Частоты встречаемости букв в %

--- src/Objects/collections/CollectionExamples.java
+++ src/Objects/collections/CollectionExamples.java
@@ -9,7 +9,7 @@ import java.util.*;
  * Created by Юлия on 07.04.2017.
  */
 public class CollectionExamples {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         //Задание 1 Shuffle
         List<Integer> l1 = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
@@ -40,25 +40,132 @@ public class CollectionExamples {
         System.out.println(Arrays.toString(s4.toArray()));
 
         //Задание 3 Количество уникальных слов в файле
-        File f=new File("D:\\lorem.txt");
-        List<String> lines= Files.readAllLines(f.toPath());
-        List<String> rezult=new ArrayList<>();
-        List<String> words=new ArrayList<>();
-        for (String s:lines){
-            s=s.toLowerCase().replaceAll("[^a-z^A-z^ ]"," ").trim();
-            s=s.replaceAll("  "," ");
-            words.addAll(Arrays.asList(s.split(" ")));
+        /*File f = new File("D:\\lorem.txt");
+        Set<String> rez = new HashSet<>();
+        List<String> words = getWordsFromFile(f);
 
-        }
-        for(String word:words){
-            if(!rezult.contains(word))
-                rezult.add(word);
-        }
-        System.out.println(rezult.size());
+        rez.addAll(words);
+        System.out.println(rez.size());*/
+
+        //--------------------
+        //Занятие 12
+        //War and Peace
+        //--------------------
+
+        File wp = new File("D:\\wp.txt");
+        //100 most popular words in War and Peace
+        popularWords(wp, 100);
+        System.out.println();
+        popularWords(wp, 10);
+        System.out.println();
+
+        //Words in groups by their's length
+        wordsInGroupsByLength(wp);
+        System.out.println();
+
+        //Top 10 the most popular word pairs
+        wordsPairs(wp, 10);
+
+        //Occurence frequency of each letter in %
+        lettersPercent(wp);
 
     }
 
-    static <T> void shuffle(List<T> list) {
+    private static Comparator<Map.Entry<String, Integer>> valuesCompare = new Comparator<Map.Entry<String, Integer>>() {
+        @Override
+        public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+            return o1.getValue() - o2.getValue();
+        }
+    };
+
+    private static List<String> getWordsFromFile(File text) throws IOException {
+        List<String> words = new ArrayList<>();
+        List<String> linesWP = Files.readAllLines(text.toPath());
+
+        for (String s : linesWP) {
+            if (s.length() > 0) {
+                s = s.toLowerCase().replaceAll("[^a-z^ ']", " ");
+                s = s.replaceAll("\\ba\\b|\\ban\\b|\\bthe\\b| '|' |^'", " ");
+                s = s.replaceAll(" +", " ").trim();
+                words.addAll(Arrays.asList(s.split(" ")));
+            }
+        }
+        return words;
+    }
+
+    private static void popularWords(File text, int topCnt) throws IOException {
+        List<String> words = getWordsFromFile(text);
+        Map<String, Integer> wordsCnt = new HashMap<>();
+        List<Map.Entry<String, Integer>> rez = new ArrayList<>();
+
+        for (String word : words) {
+            if (wordsCnt.containsKey(word))
+                wordsCnt.put(word, wordsCnt.get(word) + 1);
+            else wordsCnt.put(word, 1);
+        }
+
+        rez.addAll(wordsCnt.entrySet());
+        rez.sort(valuesCompare.reversed());
+
+        for (int i = 0; i < topCnt; i++) {
+            System.out.println(rez.get(i));
+        }
+
+    }
+
+    private static void wordsInGroupsByLength(File text) throws IOException {
+        List<String> words = getWordsFromFile(text);
+        Map<Integer, Set<String>> lengthGroups = new HashMap<>();
+
+        for (String word : words) {
+            if (!lengthGroups.containsKey(word.length()))
+                lengthGroups.put(word.length(), new HashSet<>());
+            lengthGroups.get(word.length()).add(word);
+
+        }
+        System.out.println(lengthGroups);
+    }
+
+    private static void wordsPairs(File text, int topCnt) throws IOException {
+        List<String> words = getWordsFromFile(text);
+        Map<String, Integer> pairsCnt = new HashMap<>();
+        List<Map.Entry<String, Integer>> sortedPairs = new ArrayList<>();
+
+        for (int i = 0; i < words.size() - 1; i++) {
+            String curPair = words.get(i) + " " + words.get(i + 1);
+            if (!pairsCnt.containsKey(curPair))
+                pairsCnt.put(curPair, 1);
+            else pairsCnt.put(curPair, pairsCnt.get(curPair) + 1);
+        }
+
+        sortedPairs.addAll(pairsCnt.entrySet());
+        sortedPairs.sort(valuesCompare.reversed());
+        for (int i = 0; i < topCnt; i++) {
+            System.out.println(sortedPairs.get(i));
+        }
+    }
+
+    private static void lettersPercent(File text) throws IOException {
+        Map<Character, Double> lettersCnt = new TreeMap<>();
+        List<String> lines = Files.readAllLines(text.toPath());
+        int totalCnt = 0;
+        for (String line : lines) {
+            line = line.toLowerCase().replaceAll("[^a-z]", "");
+            totalCnt += line.length();
+            char[] letters = line.toCharArray();
+            for (char c : letters) {
+                if (!lettersCnt.containsKey(c))
+                    lettersCnt.put(c, 1.0);
+                else lettersCnt.put(c, lettersCnt.get(c) + 1);
+            }
+        }
+        for (Map.Entry<Character, Double> m : lettersCnt.entrySet()) {
+            lettersCnt.put(m.getKey(), 100 * m.getValue() / totalCnt);
+        }
+        System.out.println(lettersCnt);
+    }
+
+    private static <T> void shuffle(List<T> list) {
         T tmp;
         int shuffleCnt = 0;
         boolean[] indexes = new boolean[list.size()];
@@ -86,14 +193,14 @@ public class CollectionExamples {
 
     }
 
-    static <T> Set<T> Difference(Set<T> s1, Set<T> s2) {
+    private static <T> Set<T> Difference(Set<T> s1, Set<T> s2) {
         Set<T> newSet = new HashSet<>();
         newSet.addAll(s1);
         newSet.removeAll(s2);
         return newSet;
     }
 
-    static <T> Set<T> Intersect(Set<T> s1, Set<T> s2) {
+    private static <T> Set<T> Intersect(Set<T> s1, Set<T> s2) {
         Set<T> newSet = new HashSet<>();
         newSet.addAll(s1);
         newSet.retainAll(s2);
